92072ddcc16edc051f8331cfc414dd28e40f1d56
Ridhwan Ramdhani <ridhwan@ramdhani.me>
2026-05-26 11:29:27 +0200

    feat: add Android TWA artifact workflow

---
+++ android-twa/app/src/main/java/me/ramdhani/treetales/DelegationService.java
@@ -0,0 +1,14 @@
+package me.ramdhani.treetales;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
